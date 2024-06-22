4b7d177470fff1fa866325cf26347fede3820826
Alex-Programs <alexcj2802@gmail.com>
2024-06-22 14:47:59 +0100

    Refactor

--- netcheck/src/main.rs
+++ netcheck/src/main.rs
@@ -157,7 +157,71 @@ impl App {
     }
 
     fn render_frame(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.size());
+        let area = frame.size();
+        let buf = frame.buffer_mut();
+
+        let title = Title::from(" NETCHECK ".bold());
+        let instructions = Title::from(Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]));
+        let exterior_block = Block::default()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .borders(Borders::TOP)
+            .border_set(border::THICK);
+
+        let inner_area = exterior_block.inner(area);
+        exterior_block.render(area, buf);
+
+        let BLOCK_HEIGHT = 10;
+        let BLOCK_WIDTH = 40;
+
+        let columns = inner_area.width / BLOCK_WIDTH;
+        let column_width = inner_area.width / columns;
+        let mut blocks = Vec::new();
+
+        blocks.push(self.render_network_info(inner_area));
+        blocks.push(self.render_internet_info(inner_area));
+        blocks.push(self.render_dhcp_info(inner_area));
+        blocks.push(self.render_dns_info(inner_area));
+        blocks.push(self.render_traceroute_info(inner_area));
+        blocks.push(self.render_tcp_info(inner_area));
+        blocks.push(self.render_http_info(inner_area));
+        blocks.push(self.render_https_info(inner_area));
+        blocks.push(self.render_udp_info(inner_area));
+        blocks.push(self.render_ntp_info(inner_area));
+        blocks.push(self.render_quic_info(inner_area));
+
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Length(area.height - 2), Constraint::Length(2)].as_ref())
+            .split(inner_area);
+
+        let rows = chunks[0];
+        let columns_layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(
+                (0..columns)
+                    .map(|_| Constraint::Length(column_width))
+                    .collect::<Vec<Constraint>>(),
+            )
+            .split(rows);
+
+        for (i, block) in blocks.into_iter().enumerate() {
+            let col = i % columns as usize;
+            let row = i / columns as usize;
+            if row < rows.height as usize {
+                let rect = Rect::new(
+                    columns_layout[col].x,
+                    columns_layout[col].y + row as u16 * BLOCK_HEIGHT, // Assuming each block is 10 lines high
+                    column_width,
+                    BLOCK_HEIGHT,
+                );
+                block.render(rect, buf); // Render each block directly
+            }
+        }
     }
 
     /// updates the application's state based on user input
@@ -184,192 +248,123 @@ impl App {
     fn exit(&mut self) {
         self.exit = true;
     }
-}
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Title::from(" NETCHECK ".bold());
-        let instructions = Title::from(Line::from(vec![
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]));
-        let exterior_block = Block::default()
-            .title(title.alignment(Alignment::Center))
-            .title(
-                instructions
-                    .alignment(Alignment::Center)
-                    .position(Position::Bottom),
-            )
-            .borders(Borders::TOP)
-            .border_set(border::THICK);
-
-        let inner_area = exterior_block.inner(area);
-        exterior_block.render(area, buf);
-
-        let BLOCK_HEIGHT = 10;
-        let BLOCK_WIDTH = 40;
-
-        let columns = inner_area.width / BLOCK_WIDTH;
-        let column_width = inner_area.width / columns;
-        let mut blocks = Vec::new();
-
-        blocks.push(render_network_info(&inner_area));
-        blocks.push(render_internet_info(&inner_area));
-        blocks.push(render_dhcp_info(&inner_area));
-        blocks.push(render_dns_info(&inner_area));
-        blocks.push(render_traceroute_info(&inner_area));
-        blocks.push(render_tcp_info(&inner_area));
-        blocks.push(render_http_info(&inner_area));
-        blocks.push(render_https_info(&inner_area));
-        blocks.push(render_udp_info(&inner_area));
-        blocks.push(render_ntp_info(&inner_area));
-        blocks.push(render_quic_info(&inner_area));
-
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([Constraint::Length(area.height - 2), Constraint::Length(2)].as_ref())
-            .split(inner_area);
-
-        let rows = chunks[0];
-        let columns_layout = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints::<Vec<Constraint>>(
-                (0..columns)
-                    .map(|_| Constraint::Length(column_width))
-                    .collect::<Vec<_>>(),
-            )
-            .split(rows);
-
-        let mut widgets = Vec::new();
-        for (i, block) in blocks.into_iter().enumerate() {
-            let col = i % columns as usize;
-            let row = i / columns as usize;
-            if row < rows.height as usize {
-                let rect = Rect::new(
-                    columns_layout[col].x,
-                    columns_layout[col].y + row as u16 * BLOCK_HEIGHT, // Assuming each block is 10 lines high
-                    column_width,
-                    BLOCK_HEIGHT,
-                );
-                widgets.push((block, rect));
-            }
-        }
-
-        for (widget, rect) in widgets {
-            widget.render(rect, buf); // Render each widget directly using its render method
-        }
+    fn render_network_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Local IP: 192.168.0.1"),
+            Line::from("Subnet Mask: 255.255.255.0"),
+            Line::from("Gateway: 192.168.0.254"),
+        ];
+        Paragraph::new(Text::from(text))
+            .block(Block::default().title("Network Info").borders(Borders::ALL))
     }
-}
 
-fn render_network_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Local IP: 192.168.0.1"),
-        Line::from("Subnet Mask: 255.255.255.0"),
-        Line::from("Gateway: 192.168.0.254"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("Network Info").borders(Borders::ALL))
-}
+    fn render_internet_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Public IP: 203.0.113.1"),
+            Line::from("ASN: 12345"),
+            Line::from("Reverse DNS: example.com"),
+            Line::from("ISP: Example ISP"),
+            Line::from("Location: Somewhere, Earth"),
+        ];
+        Paragraph::new(Text::from(text)).block(
+            Block::default()
+                .title("Internet Info")
+                .borders(Borders::ALL),
+        )
+    }
 
-fn render_internet_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Public IP: 203.0.113.1"),
-        Line::from("ASN: 12345"),
-        Line::from("Reverse DNS: example.com"),
-        Line::from("ISP: Example ISP"),
-        Line::from("Location: Somewhere, Earth"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("Internet Info").borders(Borders::ALL))
-}
+    fn render_dhcp_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("DHCP Server: 192.168.0.1"),
+            Line::from("Lease Time: 86400"),
+            Line::from("Last Renewed: 43200"),
+        ];
+        Paragraph::new(Text::from(text))
+            .block(Block::default().title("DHCP Info").borders(Borders::ALL))
+    }
 
-fn render_dhcp_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("DHCP Server: 192.168.0.1"),
-        Line::from("Lease Time: 86400"),
-        Line::from("Last Renewed: 43200"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("DHCP Info").borders(Borders::ALL))
-}
+    fn render_dns_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Primary DNS: 8.8.8.8"),
+            Line::from("Can Access Primary: Yes"),
+            Line::from("Secondary DNS: 8.8.4.4"),
+            Line::from("Can Access Secondary: Yes"),
+            Line::from("Tertiary DNS: 1.1.1.1"),
+            Line::from("Can Access Tertiary: Yes"),
+        ];
+        Paragraph::new(Text::from(text))
+            .block(Block::default().title("DNS Info").borders(Borders::ALL))
+    }
 
-fn render_dns_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Primary DNS: 8.8.8.8"),
-        Line::from("Can Access Primary: Yes"),
-        Line::from("Secondary DNS: 8.8.4.4"),
-        Line::from("Can Access Secondary: Yes"),
-        Line::from("Tertiary DNS: 1.1.1.1"),
-        Line::from("Can Access Tertiary: Yes"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("DNS Info").borders(Borders::ALL))
-}
+    fn render_traceroute_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Hop 1: 192.168.0.1 - Latency: 1ms"),
+            Line::from("Hop 2: 203.0.113.1 - Latency: 10ms"),
+            Line::from("Hop 3: 198.51.100.1 - Latency: 20ms"),
+        ];
+        Paragraph::new(Text::from(text)).block(
+            Block::default()
+                .title("Traceroute Info")
+                .borders(Borders::ALL),
+        )
+    }
 
-fn render_traceroute_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Hop 1: 192.168.0.1 - Latency: 1ms"),
-        Line::from("Hop 2: 203.0.113.1 - Latency: 10ms"),
-        Line::from("Hop 3: 198.51.100.1 - Latency: 20ms"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("Traceroute Info").borders(Borders::ALL))
-}
+    fn render_tcp_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Port 80: Success"),
+            Line::from("Port 443: Success"),
+            Line::from("Port 8080: Fail"),
+        ];
+        Paragraph::new(Text::from(text))
+            .block(Block::default().title("TCP Info").borders(Borders::ALL))
+    }
 
-fn render_tcp_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Port 80: Success"),
-        Line::from("Port 443: Success"),
-        Line::from("Port 8080: Fail"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("TCP Info").borders(Borders::ALL))
-}
+    fn render_http_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Can Access 1.1.1.1: Yes"),
+            Line::from("Can Access Google: Yes"),
+        ];
+        Paragraph::new(Text::from(text))
+            .block(Block::default().title("HTTP Info").borders(Borders::ALL))
+    }
 
-fn render_http_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Can Access 1.1.1.1: Yes"),
-        Line::from("Can Access Google: Yes"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("HTTP Info").borders(Borders::ALL))
-}
+    fn render_https_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Can Access 1.1.1.1: Yes"),
+            Line::from("Can Access Google: Yes"),
+        ];
+        Paragraph::new(Text::from(text))
+            .block(Block::default().title("HTTPS Info").borders(Borders::ALL))
+    }
 
-fn render_https_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Can Access 1.1.1.1: Yes"),
-        Line::from("Can Access Google: Yes"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("HTTPS Info").borders(Borders::ALL))
-}
+    fn render_udp_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Port 53: Success"),
+            Line::from("Port 123: Success"),
+        ];
+        Paragraph::new(Text::from(text))
+            .block(Block::default().title("UDP Info").borders(Borders::ALL))
+    }
 
-fn render_udp_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Port 53: Success"),
-        Line::from("Port 123: Success"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("UDP Info").borders(Borders::ALL))
-}
+    fn render_ntp_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Use NTP: Yes"),
+            Line::from("NTP Server: 129.6.15.28"),
+            Line::from("Can Access NTP: Yes"),
+            Line::from("Local Time: 1627890123"),
+            Line::from("Server Time: 1627890124"),
+        ];
+        Paragraph::new(Text::from(text))
+            .block(Block::default().title("NTP Info").borders(Borders::ALL))
+    }
 
-fn render_ntp_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Use NTP: Yes"),
-        Line::from("NTP Server: 129.6.15.28"),
-        Line::from("Can Access NTP: Yes"),
-        Line::from("Local Time: 1627890123"),
-        Line::from("Server Time: 1627890124"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("NTP Info").borders(Borders::ALL))
-}
-
-fn render_quic_info(area: &Rect) -> Paragraph {
-    let text = vec![
-        Line::from("Can Access 1.1.1.1: Yes"),
-        Line::from("Can Access Google: Yes"),
-    ];
-    Paragraph::new(Text::from(text))
-        .block(Block::default().title("QUIC Info").borders(Borders::ALL))
+    fn render_quic_info(&self, area: Rect) -> Paragraph {
+        let text = vec![
+            Line::from("Can Access 1.1.1.1: Yes"),
+            Line::from("Can Access Google: Yes"),
+        ];
+        Paragraph::new(Text::from(text))
+            .block(Block::default().title("QUIC Info").borders(Borders::ALL))
+    }
 }
